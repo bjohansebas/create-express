4919dc364a5dddebd74c5aa9b58743f562cfbc15
Sebastian Beltran <bjohansebas@gmail.com>
2024-10-15 21:11:39 -0500

    add initial files

---
+++ scripts/generate-help-docs.mjs
@@ -0,0 +1,36 @@
+import { execSync } from "node:child_process";
+import { readFile, writeFile } from "node:fs/promises";
+import { join, resolve } from "node:path";
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+	throw new Error("generate-help-docs.mjs: requires a least one parameter");
+}
+
+for (const arg of args) {
+	try {
+		const dir = resolve(arg);
+
+		let output = execSync(`node ${dir}/dist/index.js --help`, {
+			encoding: "utf-8",
+		});
+
+		output = output.replace(
+			/(?<=Usage: auth \[options\] \[command\]\n\n)[\s\S]*?(?=Options:)/,
+			"",
+		);
+
+		const readmePath = join(dir, "README.md");
+		const readme = await readFile(readmePath, "utf-8");
+
+		const updatedReadme = readme.replace(
+			/(?<=<!-- GENERATED START -->\n\n```\n)[\s\S]*?(?=```\n\n<!-- GENERATED END -->)/,
+			output,
+		);
+
+		await writeFile(readmePath, updatedReadme, "utf-8");
+	} catch (error) {
+		console.error(error);
+	}
+}
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { green } from "picocolors";
+
+import packageJson from "../package.json";
+import { checkUpdates } from "./utils/checkUpdates";
+
+export const program = new Command(packageJson.name)
+	.version(packageJson.version)
+	.usage(`${green("[command]")} ${green("[options]")}`)
+	.argument("[directory]")
+	.helpOption("-h, --help", "Display this help message.")
+	.option("--ts, --typescript", "Initialize as a TypeScript project. (default)")
+	.option("--js, --javascript", "Initialize as a JavaScript project.")
+    .option("-t, --template <template>", "Specify a template.")
+	.option("--empty", "Initialize an empty project.")
+    .option('--disable-git', "Skip initializing a git repository.")
+	.option(
+		"--skip-install",
+		"Explicitly tell the CLI to skip installing packages.",
+	)
+	.option(
+		"--use-npm",
+		"Explicitly tell the CLI to bootstrap the application using npm.",
+	)
+	.option(
+		"--use-pnpm",
+		"Explicitly tell the CLI to bootstrap the application using pnpm.",
+	)
+	.option(
+		"--use-yarn",
+		"Explicitly tell the CLI to bootstrap the application using Yarn.",
+	)
+	.option(
+		"--use-bun",
+		"Explicitly tell the CLI to bootstrap the application using Bun.",
+	)
+	.hook("postAction", async () => {
+		await checkUpdates();
+	});
+
+program.parse(process.argv);
